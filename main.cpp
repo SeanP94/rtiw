6990f37ad8dbc7723fd95b109476ff752ec52a71
perry094 <seanep94@gmail.com>
2022-01-23 15:24:47 -0800

    3rd.. still getting used to git..

--- main.cpp
+++ main.cpp
@@ -1,29 +1,46 @@
 #include <iostream>
+#include "vec3d.h"
+#include "color.h"
+#include "ray.h"
 
+//This creates a simple ray casted gradient background.
+color ray_background_gradient(const ray& r) {
+	vec3d unit_direction = unit_vector(r.direction());
+	auto t = 0.5*(unit_direction.y() + 1.0);
+	return (1.0-t)*color(1.0,1.0,1.0) + t*color(0.5,0.7,1.0);
+}
 
 int main() {
-	//Set size of img
-	const int img_width = 256;
-	const int img_height = 256;
-	//Setting P3 are set to this standard:
-	//ASCII 3 columns, 2 rows.
+	auto ray_fun = ray_background_gradient;
+	//Image data:
+	const auto aspect_ratio = 16.0 / 9.0;
+	const int img_width = 400;
+	const int img_height = static_cast<int>(img_width / aspect_ratio);
+
+	//Camera data:
+	const auto viewport_height = 2.0;
+	auto viewport_width = aspect_ratio * viewport_height;
+	auto focal_length = 1.0;
+
+	//Origin of camera
+	auto origin = point3(0,0,0);
+	auto horiz  = vec3d(viewport_width,0,0);
+	auto vert   = vec3d(0,viewport_height,0);
+	auto lower_left_corner = origin - horiz/2 - vert/2 - vec3d(0,0,focal_length);
+
+	//Rendering:
 	std::cout << "P3\n" << img_width <<  ' ' << img_height << "\n255\n";
 	
 	//Loop to create the image
 	for (int i = img_height-1; i >= 0; --i) {
 		//Create a Progress indicator.
 		std::cerr << "\rScalines remaining: " << i << ' ' << std::flush;
-
 		for (int j = 0; j < img_width; ++j) {
-			auto r = double(j) / (img_width -1);
-			auto g = double(i) / (img_height-1);			
-			auto b = 0.25;
-
-			int ir = static_cast<int>(255.999 * r);
-			int ig = static_cast<int>(255.999 * g);
-			int ib = static_cast<int>(255.999 * b);
-
-			std::cout << ir << ' ' << ig << ' ' << ib << std::endl;	
+			auto u = double(j) / (img_width-1);
+			auto v = double(i) / (img_height-1);
+			ray r(origin, (lower_left_corner + u*horiz + v*vert - origin));
+			color pixel_color = ray_fun(r);
+			write_color(std::cout, pixel_color);
 		}
 	}
 	std::cerr << "\nFile complete.\n";
